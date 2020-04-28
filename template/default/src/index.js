2c7d7e704632383f5c767360b99495c386725505
John Roberts <john.calvin.roberts@gmail.com>
2020-04-28 11:12:51 +0800

    fix: hydrate

--- template/default/src/index.js
+++ template/default/src/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+window.env = process.env.NODE_ENV;
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
