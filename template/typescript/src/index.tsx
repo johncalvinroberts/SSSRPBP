75954eb830cc4c40bb265a358c16622230a3a327
John Roberts <john.calvin.roberts@gmail.com>
2020-08-29 18:44:54 +0800

    feat: add typescript template

---
+++ template/typescript/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
